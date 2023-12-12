27feeefeffa42f83ac3c877012024b141eb64914
ViacheslavMelnikov <melnikov.plusm@yandex.ru>
2023-12-12 08:40:46 +0700

    After 2 review 18 sprint

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -8,33 +8,24 @@ from pep_parse.settings import BASE_DIR
 class PepParsePipeline:
 
     def open_spider(self, spider):
-        self.status_summary_dictionary = defaultdict(int)
+        self.status_summary_dict = defaultdict(int)
 
     def process_item(self, item, spider):
-        self.status_summary_dictionary[item['status']] += 1
+        self.status_summary_dict[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        self.status_summary_dictionary['Total'] = sum(
-            self.status_summary_dictionary.values())
+        self.status_summary_dict['Total'] = sum(
+            self.status_summary_dict.values())
 
         date_now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
         name_file = f'{BASE_DIR}/results/status_summary_{date_now}.csv'
         with open(name_file, 'w', newline='') as output_file:
-            # этот вариант мне больше нравится чем вариант ниже
-
-            # output_file.write('Статус,Количество\n')
-            # output_file.write(
-            #     (''.join(
-            #         [f'{key},{self.status_summary_dictionary[key]}\n'
-            #          for key in self.status_summary_dictionary.keys()])))
-
             fieldnames = ['Статус', 'Количество']
             dict_writer = csv.DictWriter(output_file, fieldnames)
             dict_writer.writeheader()
             dict_writer.writerows(
                 [
-                    {
-                        'Статус': key,
-                        'Количество': self.status_summary_dictionary[key]
-                    } for key in self.status_summary_dictionary.keys()])
+                    {'Статус': key,
+                     'Количество': value
+                     } for key, value in self.status_summary_dict.items()])
